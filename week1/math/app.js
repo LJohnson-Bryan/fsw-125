0ad8b8f9de226dc40696924d5c8b4f3fd51598d1
LJohnson-Bryan <logan.johnson@bryanuniversity.edu>
2020-11-20 09:55:27 -0500

    Modular Math

---
+++ week1/math/app.js
@@ -0,0 +1,9 @@
+const { add, subtract, multiply, divide } = require('./math.js');
+
+console.log(`Addition: ${add(10, 20)}`); // 30
+
+console.log(`Subtraction: ${subtract(30, 20)}`); // 10
+
+console.log(`Multiplication: ${multiply(10, 2)}`); // 20
+
+console.log(`Division: ${divide(10, 2)}`); // 5
